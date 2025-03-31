68f63906871f38be49ff3db15059bc20d969df3b
Vana <savych.vana@gmail.com>
2025-03-31 16:16:27 +0300

    hot fix

--- app/Administration/page.tsx
+++ app/Administration/page.tsx
@@ -3,8 +3,8 @@ export default function Tab5Page() {
   return (
     <div className="container mx-auto p-4">
       <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Post_Office</h1>
-        <p>This is the content for Tab Post_Office.</p>
+        <h1 className="text-2xl font-bold mb-4">Administrations</h1>
+        <p>This is the content for Tab Administrations.</p>
       </div>
     </div>
   );
